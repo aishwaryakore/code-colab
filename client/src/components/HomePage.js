fc76b141a30423aad8d8b680d5782a6f17bd09ee
aishwaryakore <aishwaryakore07@gmail.com>
2025-08-28 18:13:42 -0400

    Home page UI

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -2,8 +2,51 @@ import React from 'react'
 
 const HomePage = () => {
     return (
-        <div>
-            Home Page
+        <div className="container-fluid">
+            <div className="row justify-content-center align-items-center min-vh-100">
+                <div className="col-12 col-md-6">
+                    <div className="card shadow-sm p-2 mb-5 bg-secondary rounded">
+                        <div className="card-body text-center bg-dark">
+                            <img
+                                src="/images/codecast.png"
+                                alt="Logo"
+                                className="img-fluid mx-auto d-block"
+                                style={{ maxWidth: "150px" }}
+                            />
+                            <h4 className="card-title text-light mb-4">Enter the ROOM ID</h4>
+                            <div className="form-group">
+                                <input
+                                    type="text"
+                                    className="form-control mb-2"
+                                    placeholder="ROOM ID"
+                                />
+                            </div>
+                            <div className="form-group">
+                                <input
+                                    type="text"
+                                    className="form-control mb-2"
+                                    placeholder="USERNAME"
+                                />
+                            </div>
+                            <button className="btn btn-success btn-lg btn-block">
+                                JOIN
+                            </button>
+                            <p className="mt-3 text-light">
+                                Don't have a room ID? create{" "}
+                                <span
+                                    className=" text-success p-2"
+                                    style={{ cursor: "pointer" }}
+                                >
+                                    {" "}
+                                    New Room
+                                </span>
+                            </p>
+                        </div>
+
+                    </div>
+                </div>
+
+            </div>
         </div>
     )
 
